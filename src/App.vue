da5c5e255c750894a42f17afc3a94ec602087c74
zhscv <zhouhansh@gmail.com>
2021-09-06 20:39:09 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tab-bar-main></tab-bar-main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TabBarMain from "@/components/common/mainTabbar/TabBarMain";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TabBarMain
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "assets/css/base.css";
 </style>
